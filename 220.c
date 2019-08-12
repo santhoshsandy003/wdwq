362630d4fd78ad20b9982679b73d2a02134bf8ad
santhoshsandy003 <48909585+santhoshsandy003@users.noreply.github.com>
2019-08-12 21:11:57 +0530

    Create 220.c

---
+++ 220.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int nw,i=1;
+    printf("Enter an integer: ");
+    scanf("%d",&nw);
+    for(i=1;i<nw;++i)
+    {
+      printf("\n 5 * %ld = %ld",i,5*i);
+    }
+   
+    
+    
+}
