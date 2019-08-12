9b76fc2b92ac139f81211c37e9a72a30a1d52fb1
santhoshsandy003 <48909585+santhoshsandy003@users.noreply.github.com>
2019-08-12 21:16:00 +0530

    Create 3a.c

---
+++ 3a.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main() {
+  int m,v,c,i,sum=0;
+  printf("enter the value",m,v,c);
+  scanf("%d" "%d" "%d",&n,&v, &c);
+  for(i=m;i>1;i--)
+  {
+  sum=sum+(v+(m-1)*c);
+  }
+}
